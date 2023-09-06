2f9f70f66ebae5b8587069c8b208bc17915f52b0
AidanVazq Vazquez <avr.25dogs@gmail.com>
2023-09-06 20:25:41 +0000

    zest

--- GitPractice.py
+++ GitPractice.py
@@ -5,6 +5,8 @@ toDoList.append(item)
    # Finish this code
    return toDoList
 
+   def deleteItem(item)
+
 userAns = input("Do you want to add to your list or quit? A/Q")
 while userAns == "A":
    item = input("What item do you want to add?")
